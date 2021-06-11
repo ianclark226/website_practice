febb26528e2140031f37a75716890a604d65655f
Ian Clark <ianclark226@gmail.com>
2021-06-11 13:10:26 -0400

    mapbox and modal

---
+++ Bootstrap_website/script.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiaWFuY2xhcmsyMjYiLCJhIjoiY2s5YmljdXBzMDNuZzNvcDdndTFnNjRweCJ9.Cgq-sfxAxABuf2peE4_i5Q';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
+center: [-71.060982, 42.35725],
+zoom: 18,
+});
